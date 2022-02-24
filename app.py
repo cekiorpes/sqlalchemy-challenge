3429327d99f5b9607a129d055be5573763a957a5
Caroline Kiorpes <cekiorpes@gmail.com>
2022-02-23 20:46:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ Measurement = Base.classes.measurement
 app = Flask(__name__)
 
 #Flask routes
+#Home
 @app.route("/")
 def home():
     return (
@@ -37,6 +38,7 @@ def home():
         f"/api/v1.0/<start>/<end><br/>"
     )
 
+#Precipitation
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create our session (link) from Python to the DB
@@ -47,7 +49,6 @@ def precipitation():
     results = session.query(Measurement.date, Measurement.prcp).all()
 
     session.close()
-    print(results)
 
     # Create a dictionary from the row data and append to a list
     precipitation = []
@@ -59,6 +60,7 @@ def precipitation():
 
     return jsonify(precipitation)
 
+#Stations
 @app.route("/api/v1.0/stations")
 def stations():
     # Create our session (link) from Python to the DB
@@ -74,6 +76,38 @@ def stations():
     all_stations = list(np.ravel(results))
 
     return jsonify(all_stations)
+
+#Temperature
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of dates and tobs for the most active station for the last year of data"""
+    most_active_station = 'USC00519281'
+    most_recent_year = '2016-08-23'
+
+    #Query
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == most_active_station).\
+        filter(Measurement.date >= most_recent_year).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list
+    temp = []
+    for date, tobs in results:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        temp.append(tobs_dict)
+
+    return jsonify(temp)
+
+#Start
+
+#Start and end
+
 
 if __name__ == '__main__':
     app.run(debug=True)
