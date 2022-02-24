0493f4a22da51bf4b164fdaa9cd79c92ad3bd9bc
Caroline Kiorpes <cekiorpes@gmail.com>
2022-02-23 20:09:49 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+#Import dependencies
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Database Setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Station = Base.classes.station
+Measurement = Base.classes.measurement
+
+# Flask Setup
+app = Flask(__name__)
+
+#Flask routes
+@app.route("/")
+def home():
+    return (
+        "Welcome!<br/>"
+        "Available routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
